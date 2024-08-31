0f1bf7274de30a5203a84ff5b70c5dbebf59bda1
Michelle Lang Baratto <mi.baratto@gmail.com>
2024-08-31 21:37:34 +0200

    Project HTML/CSS Redi

---
+++ HTML_CSS_PetShopProject/app.js
@@ -0,0 +1,14 @@
+// document.addEventListener("click", function () {
+//   console.log("Hellow!");
+// });
+
+// document.addEventListener("click", function () {
+//   const mainHeadign = document.querySelector("container");
+//   mainHeadign.style.background = "red";
+// });
+
+// const price = document.querySelector(".product-price").innerHTML;
+// console.log(price);
+
+const priceInCart = document.querySelector(".price-cart").innerHTML;
+console.log(priceInCart);
